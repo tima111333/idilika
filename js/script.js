52cf6130949aa6072f817cb51029f495f2a9af6e
Timur <super.tima2001@mail.ru>
2021-08-10 16:27:54 +0300

    jndfjn

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const heart = document.querySelectorAll('.heart-svg');
 $(document).ready(function () {
   $('.slider').slick({
     arrows: true,
@@ -5,6 +6,7 @@ $(document).ready(function () {
     speed:1500
   });
   $('.slider2').slick({
+    arrows:true,
     slidesToShow: 3,
     slidesToScroll: 1,
     adaptiveHeight:true,
@@ -14,13 +16,20 @@ $(document).ready(function () {
   });
   $('.slider3').slick({
     slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
     adaptiveHeight:true,
     adaptiveWidth:true,
     speed:1500,
     variableWidth:true
   });
 });
+heart.forEach(function(item){
+ item.addEventListener('click', function(){
+   const path = item.querySelector('path');
+   
+    path.classList.toggle('heart-path');
+    
+});
+ });
 
-  
  
